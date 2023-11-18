c0a45484e46a708b197841b708d6ce77213e0431
Manjush <133102637+LordManjush@users.noreply.github.com>
2023-11-18 16:05:27 +0530

    Add files via upload

---
+++ limbo.h
@@ -0,0 +1,60 @@
+#include <string>
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <vector>
+
+
+HANDLE col;
+std::vector<std::string> logs;
+std::fstream file;
+
+namespace Limbo
+{
+
+void Save(std::string FileName)
+{
+    if(FileName.empty())
+    {
+        FileName = "Limbo.log";
+    }
+    file.open(FileName, std::ios_base::out);
+    if(file.is_open())
+    {
+        file << "";
+        file.close();
+    }
+    file.open(FileName, std::ios_base::app);
+    if(file.is_open())
+    {
+        for (auto& s : logs) {
+            file << s + "\n";
+        }
+
+        file.close();
+    }
+}
+
+    void init()
+    {
+        col = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleTextAttribute(col, 7);
+
+
+    }
+    void ErrorLog(std::string message) {
+        SetConsoleTextAttribute(col, 4);
+        std::cout << "[Error] " + message << std::endl;
+        logs.push_back("[Error] " + message);
+    }
+    void WarningLog(std::string message) {
+        SetConsoleTextAttribute(col, 6);
+        std::cout << "[Warning] " + message << std::endl;
+        logs.push_back("[Warning] " + message);
+    }
+    void SucessLog(std::string message) {
+        SetConsoleTextAttribute(col, 2);
+        std::cout << "[Success] " + message << std::endl;
+        logs.push_back("[Success] " + message);
+    }
+}
