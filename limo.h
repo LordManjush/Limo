7c67a4b90a93e7b246d221a8dd7aab2e1b81e584
Manjush <133102637+LordManjush@users.noreply.github.com>
2023-11-20 11:26:43 +0530

    Update limo.h

--- limo.h
+++ limo.h
@@ -6,48 +6,47 @@
 
 
 HANDLE col;
-std::vector<std::string> logs;
+
 std::fstream file;
 
 namespace Limo
 {
-
-void Save(std::string FileName)
-{
-    if(FileName.empty())
+    std::vector<std::string> logs;
+    void Save(std::string FileName)
     {
-        FileName = "Limbo.log";
-    }
-    file.open(FileName, std::ios_base::out);
-    if(file.is_open())
-    {
-        file << "";
-        file.close();
-    }
-    file.open(FileName, std::ios_base::app);
-    if(file.is_open())
-    {
-        for (auto& s : logs) {
-            file << s + "\n";
+        if(FileName.empty())
+        {
+            FileName = "Limbo.log";
         }
+        file.open(FileName, std::ios_base::out);
+        if(file.is_open())
+        {
+            file << "";
+            file.close();
+        }
+        file.open(FileName, std::ios_base::app);
+        if(file.is_open())
+        {
+            for (auto& s : logs) {
+                file << s + "\n";
+            }
 
-        file.close();
+            file.close();
+        }
     }
-}
 
     void init()
     {
         col = GetStdHandle(STD_OUTPUT_HANDLE);
         SetConsoleTextAttribute(col, 7);
-
-
+        logs.clear();
     }
     void ErrorLog(std::string message) {
         SetConsoleTextAttribute(col, 4);
         std::cout << "[Error] " + message << std::endl;
         logs.push_back("[Error] " + message);
     }
-void InfoLog(std::string message) {
+    void InfoLog(std::string message) {
         SetConsoleTextAttribute(col, 1);
         std::cout << "[Info] " + message << std::endl;
         logs.push_back("[Info] " + message);
